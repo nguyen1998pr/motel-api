b172c8a5f7bf8423933b29f764a8b5b30b808201
nguyen1998pr <55442292+nguyen1998pr@users.noreply.github.com>
2021-10-11 16:02:20 +0700

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+//Initial MongoDB
+
+module.exports = {
+  database: "mongodb://localhost:27017/motel-demo", //DB source
+  secret: "nguyenvd3",
+};
---
+++ models/property.js
@@ -0,0 +1,11 @@
+// import module
+const mongoose = require("mongoose");
+
+// import DB
+const config = require("../config/database");
+
+// create schema
+var Schema = mongoose.Schema;
+
+// Define property for Schema
+const PropertySchema = mongoose.Schema({});
